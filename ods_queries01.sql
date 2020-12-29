33873d13fa64d9ee4c2287a1cf8f47dc6e972ea6
IbraKadabra <skipperswede@gmail.com>
2020-12-29 12:26:01 -0800

    Update ods_queries01.sql

--- ods_queries01.sql
+++ ods_queries01.sql
@@ -16,17 +16,6 @@ SELECT TOP (1000) [Id]
   FROM [WebAPIConsumer].[dbo].[AspNetRoles] where Id IN (
 'f96e7d75-0fde-42d2-a42c-8dfa02cc376d',
 'de39ce09-4114-48b8-83d2-deac0c226df7',
-'dd642b7b-7007-4c47-a351-3a182b037158',
-'c60ca1cf-f1f6-4000-97b7-9de5ba1e83f1',
-'932fb565-b0c3-485e-b56a-9ddd9981f185',
-'8b75cab6-14fa-46e1-b64d-6026b796d7c1',
-'70e6962b-1eb4-47ba-8b49-ee790accb633',
-'68f0d4ff-f0e4-452a-998a-a79022b8b967',
-'61f2d582-c6f7-437f-a01c-c64cf46753ed',
-'5fcea5aa-508e-41ab-a133-e1de9a168e50',
-'4b89c771-778b-46e2-abc3-fdd178df3868',
-'2e42daeb-bcae-4776-9da7-25532537c1dc',
-'2ca45248-0c19-4441-ba8e-d1102027e2d8',
 '0e26e4db-e8be-4bae-ac6b-2d813ed64d64'
 )
 
@@ -53,7 +42,7 @@ where UserId IN('0317f08a-8fc7-46db-bfe1-ddf****')
 
 
 
-----[From dba]------------------------------------------------------
+----[status codes and descriptions]---------------------------------
 
 /*
               a. 10 = update successful
@@ -92,14 +81,14 @@ select * from sys.transmission_queue with (nolock)
 order by enqueue_time desc;
 
 
-----Succeeded (10); MessageLogID = 126 on 12/13/2019
+----Succeeded (10); MessageLogID = 126 on 02/12/2019
 --<root>
 --  <messageMetadata messageInterfaceID="1228" messageRecipient="source" orignateServerName="DEVXXX" isCheckConcurrency="0" />
---  <updated col1="E33EACBC-0E1E-EA11-80FF-0015***" col2="1069" col3="0" col5="0" col6="1" col11="Alexander" col13="Kovacic" col14="820 W Foothill Blvd" col17="Upland" col18="CA" col20="91786" col23="9094005633" col25="0" col27="1" col29="Alexander" col31="Kovacic" col32="820 W Foothill Blvd" col35="Upland" col36="CA" col38="91786" col41="9094005633" col43="0" col50="1" col52="1" col57="2019-12-13T17:12:04.200-08:00" col58="2019-12-13T17:12:04.200-08:00" col59="DEV04DMZ1" col60="DEV04DMZ1" col61="1069" col62="1069" col63="ODS004Q" col64="4" col65="4" col66="1" col67="0" col68="3" col69="10690" col70="4" col71="2019-12-13T00:00:00Z" col72="0" col45="2019-12-13T17:12:04.200" col46="2019-12-13T17:12:04.200" col51="2019-12-13T17:12:00" />
+--  <updated col1="E33EACBC-0E1E-EA11-80FF-0015***" col2="1069" col3="0" col5="0" col6="1" col11="Alexander" col13="Kovacic" col14="820 W Foothill Blvd" col17="Upland" col18="CA" col20="91786" col23="9094005633" col25="0" col27="1" col29="Alexander" col31="Kovacic" col32="820 W Foothill Blvd" col35="Upland" col36="CA" col38="91786" col41="9094005633" col43="0" col50="1" col52="1" col57="2019-12-13T17:12:04.200-08:00" col58="2019-12-13T17:12:04.200-08:00" col59="DEV04DMZ1" col60="DEV04DMZ1" col61="1069" col62="1069" col63="ODS004Q" col64="4" col65="4" col66="1" col67="0" col68="3" col69="10690" col70="4" col71="2019-02-13T00:00:00Z" col72="0" col45="2019-02-13T17:12:04.200" col46="2019-02-13T17:12:04.200" col51="2019-02-13T17:12:00" />
 --</root>
 
-----Sent (1); MessageLogID = 145 on 12/18/2019
+----Sent (1); MessageLogID = 145 on 02/18/2019
 --<root>
 --  <messageMetadata messageInterfaceID="1228" messageRecipient="source" orignateServerName="DEVXXX" isCheckConcurrency="0" />
---  <updated col1="1CEE1702-DE21-EA11-80FF-0015****" col2="1069" col3="0" col5="0" col6="1" col11="Emil" col13="Forsberg" col14="1721 W 11th St" col17="Upland" col18="CA" col20="91786" col23="9099857901" col25="0" col27="1" col29="Emil" col31="Forsberg" col32="1721 W 11th St" col35="Upland" col36="CA" col38="91786" col41="9099857901" col43="0" col50="1" col52="1" col57="2019-12-18T13:33:19.980-08:00" col58="2019-12-18T13:33:19.980-08:00" col59="DEV04DMZ1" col60="DEV04DMZ1" col61="1069" col62="1069" col63="ODS004U" col64="4" col65="4" col66="1" col67="0" col68="3" col69="10690" col70="2" col71="2019-12-18T00:00:00Z" col72="0" col45="2019-12-18T13:33:19.980" col46="2019-12-18T13:33:19.980" col51="2019-12-18T13:33:00" />
+--  <updated col1="1CEE1702-DE21-EA11-80FF-0015****" col2="1069" col3="0" col5="0" col6="1" col11="Emil" col13="Forsberg" col14="1721 W 11th St" col17="Upland" col18="CA" col20="91786" col23="9099857901" col25="0" col27="1" col29="Emil" col31="Forsberg" col32="1721 W 11th St" col35="Upland" col36="CA" col38="91786" col41="9099857901" col43="0" col50="1" col52="1" col57="2019-12-18T13:33:19.980-08:00" col58="2019-12-18T13:33:19.980-08:00" col59="DEV04DMZ1" col60="DEV04DMZ1" col61="1069" col62="1069" col63="ODS004U" col64="4" col65="4" col66="1" col67="0" col68="3" col69="10690" col70="2" col71="2019-02-18T00:00:00Z" col72="0" col45="2019-02-18T13:33:19.980" col46="2019-02-18T13:33:19.980" col51="2019-02-18T13:33:00" />
 --</root>
